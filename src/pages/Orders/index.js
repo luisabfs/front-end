a7a0da7f087130c3a485678deff65c838fe59970
Luísa Barros <qlullyq@gmail.com>
2019-06-27 00:05:54 -0300

    feat(orders): add orders page

---
+++ src/pages/Orders/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Header from '../../components/Header';
+
+// import { Container } from './styles';
+
+export default class Orders extends Component {
+  render() {
+    return <Header />;
+  }
+}
